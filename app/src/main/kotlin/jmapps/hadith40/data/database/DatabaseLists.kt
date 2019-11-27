3a929596db4b964231cf6fe4ae84ff9d9dd0f93b
dev_muslim <jmappsmuslim@gmail.com>
2019-11-27 21:02:31 +0300

    Create database favorite list

--- app/src/main/kotlin/jmapps/hadith40/data/database/DatabaseLists.kt
+++ app/src/main/kotlin/jmapps/hadith40/data/database/DatabaseLists.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import jmapps.hadith40.presentation.ui.chapters.ModelChapter
+import jmapps.hadith40.presentation.ui.favorites.ModelFavorite
 
 class DatabaseLists(private val context: Context?) {
 
@@ -41,5 +42,38 @@ class DatabaseLists(private val context: Context?) {
                 }
             }
             return chapterList
+        }
+
+    val getFavroiteList: MutableList<ModelFavorite>
+        @SuppressLint("Recycle")
+        get() {
+
+            database = DatabaseOpenHelper(context).readableDatabase
+
+            val cursor: Cursor = database.query(
+                "Table_of_chapters",
+                null,
+                "Favorite = 1",
+                null,
+                null,
+                null,
+                null)
+
+            val favoriteList = ArrayList<ModelFavorite>()
+
+            if (cursor.moveToFirst()) {
+                while (!cursor.isAfterLast) {
+                    val favorites = ModelFavorite(
+                        cursor.getInt(cursor.getColumnIndex("_id")),
+                        cursor.getString(cursor.getColumnIndex("NumberHadeeth")),
+                        cursor.getString(cursor.getColumnIndex("TitleHadeeth")))
+                    favoriteList.add(favorites)
+                    cursor.moveToNext()
+                    if (cursor.isClosed) {
+                        cursor.close()
+                    }
+                }
+            }
+            return favoriteList
         }
 }
