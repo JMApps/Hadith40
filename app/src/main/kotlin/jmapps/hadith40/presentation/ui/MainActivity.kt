42115c88aedd0af5ffc3ad6910603205f4cf28c5
dev_muslim <jmappsmuslim@gmail.com>
2019-11-27 08:37:45 +0300

    Added main presenter with change night mode

--- app/src/main/kotlin/jmapps/hadith40/presentation/ui/MainActivity.kt
+++ app/src/main/kotlin/jmapps/hadith40/presentation/ui/MainActivity.kt
@@ -1,27 +1,46 @@
 package jmapps.hadith40.presentation.ui
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Switch
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.preference.PreferenceManager
 import com.google.android.material.navigation.NavigationView
 import jmapps.hadith40.R
+import jmapps.hadith40.presentation.mvp.main.MainContract
+import jmapps.hadith40.presentation.mvp.main.MainPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    TextWatcher {
+    TextWatcher, MainContract.View {
 
+    private var keyNightMode = "key_night_mode"
+
+    private var mainPresenter: MainPresenter? = null
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    private lateinit var swNightMode: Switch
+
+    @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        editor = preferences.edit()
 
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar,
@@ -29,11 +48,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.string.navigation_drawer_close
         )
 
+        mainPresenter = MainPresenter(this)
+
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navigationView.setNavigationItemSelectedListener(this)
 
         etSearchByChapters.addTextChangedListener(this)
+
+        navigationView.menu.findItem(R.id.nav_night_mode).actionView = Switch(this)
+        swNightMode = navigationView.menu.findItem(R.id.nav_night_mode).actionView as Switch
+        swNightMode.isClickable = false
+        swNightMode.isChecked = preferences.getBoolean(keyNightMode, false)
+        initView()
     }
 
     override fun onBackPressed() {
@@ -51,9 +78,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-
+            R.id.nav_night_mode -> {
+                mainPresenter?.getNightMode(!swNightMode.isChecked)
+                swNightMode.isChecked = !swNightMode.isChecked
+            }
         }
-
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
@@ -65,4 +94,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+    override fun initView() {
+        mainPresenter?.getNightMode(swNightMode.isChecked)
+    }
+
+    override fun saveNigtModeState(state: Boolean) {
+        editor.putBoolean(keyNightMode, state).apply()
+    }
 }
