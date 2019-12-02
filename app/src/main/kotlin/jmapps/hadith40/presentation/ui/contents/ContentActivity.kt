bb2f31e3295512f9f580df089bee0a7a241eec46
dev_muslim <jmappsmuslim@gmail.com>
2019-12-02 09:34:49 +0300

    Added player with favorite buttons and logic

--- app/src/main/kotlin/jmapps/hadith40/presentation/ui/contents/ContentActivity.kt
+++ app/src/main/kotlin/jmapps/hadith40/presentation/ui/contents/ContentActivity.kt
@@ -1,23 +1,59 @@
 package jmapps.hadith40.presentation.ui.contents
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.database.sqlite.SQLiteDatabase
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.text.Html
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment.STYLE_NORMAL
+import androidx.preference.PreferenceManager
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.appbar.AppBarLayout
 import jmapps.hadith40.R
+import jmapps.hadith40.data.database.DatabaseContent
 import jmapps.hadith40.data.database.DatabaseLists
-import jmapps.hadith40.presentation.ui.apart.ApartBottomSheet
+import jmapps.hadith40.data.database.DatabaseOpenHelper
+import jmapps.hadith40.presentation.mvp.chapters.ChapterContract
+import jmapps.hadith40.presentation.mvp.chapters.ChapterPresenter
+import jmapps.hadith40.presentation.ui.apart.ApartActivity
 import jmapps.hadith40.presentation.ui.chapters.ModelChapter
 import kotlinx.android.synthetic.main.activity_content.*
 
 class ContentActivity : AppCompatActivity(), ViewPager.OnPageChangeListener,
-    AppBarLayout.OnOffsetChangedListener, View.OnClickListener {
+    View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener,
+    ChapterContract.ChapterView, MediaPlayer.OnCompletionListener {
+
+    private var database: SQLiteDatabase? = null
 
     private lateinit var chapterList: MutableList<ModelChapter>
+    private lateinit var contentList: MutableList<ModelContent>
 
+    private var chapterPresenter: ChapterPresenter? = null
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    private var player: MediaPlayer? = null
+    private lateinit var runnable: Runnable
+    private var handler: Handler = Handler()
+
+    private var trackIndex: Int? = null
+    private var chapterPosition: Int? = null
+
+    private var numberHadeeth: String? = null
+    private var chapterTitle: String? = null
+    private var contentArabic: String? = null
+    private var contentTranslation: String? = null
+
+    @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
@@ -26,52 +62,208 @@ class ContentActivity : AppCompatActivity(), ViewPager.OnPageChangeListener,
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val chapterPosition = intent.getIntExtra("key_chapter_position", 0)
+        database = DatabaseOpenHelper(this).readableDatabase
+
         chapterList = DatabaseLists(this).getChapterList
+        contentList = DatabaseContent(this).getContentList
+
+        chapterPresenter = ChapterPresenter(this, database!!)
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        editor = preferences.edit()
 
         val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         mainViewPager.adapter = sectionsPagerAdapter
 
-        tvChapterNumber.text = chapterList[chapterPosition - 1].strNumberHadeeth
-        tvChapterTitle.text = chapterList[chapterPosition - 1].strChapterTitle
-        mainViewPager.currentItem = chapterPosition - 1
+        chapterPosition = intent.getIntExtra("key_chapter_position", 0)
+        mainViewPager.currentItem = chapterPosition!! - 1
 
-        appBar.addOnOffsetChangedListener(this)
+        numberHadeeth = chapterList[chapterPosition!! - 1].strNumberHadeeth
+        chapterTitle = chapterList[chapterPosition!! - 1].strChapterTitle
+
+        contentArabic = contentList[chapterPosition!! - 1].strContentArabic
+        contentTranslation = contentList[chapterPosition!! - 1].strContentTranslation
+
+        tvChapterNumber.text = numberHadeeth
+        tvChapterTitle.text = chapterTitle
+
         mainViewPager.addOnPageChangeListener(this)
-        fabApartContent.setOnClickListener(this)
+
+        tbPlay.setOnCheckedChangeListener(this)
+        sbAudioProgress.setOnSeekBarChangeListener(this)
+        tbLoop.setOnCheckedChangeListener(this)
+
+        tbChapterFavorite.isChecked =
+            preferences.getBoolean("key_chapter_favorite_${chapterPosition}", false)
+        btnShareContent.setOnClickListener(this)
+        tbChapterFavorite.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.content, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+
+            R.id.action_apart -> {
+                val toApartList = Intent(this, ApartActivity::class.java)
+                toApartList.putExtra("key_apart_position", chapterPosition)
+                startActivity(toApartList)
+            }
+
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onPageScrollStateChanged(state: Int) {
+    override fun onPageScrollStateChanged(state: Int) {}
 
-    }
-
-    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-    }
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
 
     override fun onPageSelected(position: Int) {
-        tvChapterNumber.text = chapterList[position].strNumberHadeeth
-        tvChapterTitle.text = chapterList[position].strChapterTitle
+        chapterPosition = position + 1
+
+        numberHadeeth = chapterList[position].strNumberHadeeth
+        chapterTitle = chapterList[position].strChapterTitle
+
+        contentArabic = contentList[position].strContentArabic
+        contentTranslation = contentList[position].strContentTranslation
+
+        tvChapterNumber.text = numberHadeeth
+        tvChapterTitle.text = chapterTitle
     }
 
-    override fun onOffsetChanged(appBar: AppBarLayout?, verticalOffset: Int) {
-        if (verticalOffset < 0) {
-            fabApartContent.hide()
-        } else {
-            fabApartContent.show()
+    override fun onDestroy() {
+        super.onDestroy()
+        if (player != null) {
+            handler.removeCallbacks(runnable)
         }
+        clear()
     }
 
     override fun onClick(v: View?) {
-        val apartBottomSheet = ApartBottomSheet()
-        apartBottomSheet.setStyle(STYLE_NORMAL, R.style.BottomSheetStyleFull)
-        apartBottomSheet.show(supportFragmentManager, "apart_list")
+        when (v?.id) {
+
+            R.id.btnPrevious -> {
+
+            }
+
+            R.id.btnNext -> {
+
+            }
+
+            R.id.btnShareContent -> {
+                val shareContent = Intent(Intent.ACTION_SEND)
+                shareContent.type = "text/plain"
+                shareContent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(
+                        "$numberHadeeth<br/>$chapterTitle<p/>" +
+                                "$contentArabic<p/>$contentTranslation").toString())
+                startActivity(shareContent)
+            }
+        }
     }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        when (buttonView?.id) {
+
+            R.id.tbChapterFavorite -> chapterPresenter?.getFavorite(isChecked, chapterPosition!!)
+
+            R.id.tbPlay -> {
+                if (isChecked) {
+                    if (player == null) {
+                        initPlayer(chapterPosition!!)
+                        player?.start()
+                    } else {
+                        currentAudioProgress()
+                        player?.start()
+                    }
+                } else {
+                    player?.pause()
+                }
+            }
+
+            R.id.tbLoop -> {
+                if (isChecked) {
+                    Toast.makeText(this, getString(R.string.player_loop_on), Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this, getString(R.string.player_loop_off), Toast.LENGTH_SHORT)
+                        .show()
+                }
+                player?.isLooping = isChecked
+            }
+        }
+    }
+
+    override fun saveFavorite(keyFavorite: String, state: Boolean) {
+        editor.putBoolean(keyFavorite, state).apply()
+    }
+
+    override fun saveMessage(state: Boolean) {
+        if (state) {
+            Toast.makeText(this, getString(R.string.action_favorite_added), Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            Toast.makeText(this, getString(R.string.action_favorite_removed), Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    override fun showMessage(message: String) {
+        Toast.makeText(this, "${getString(R.string.action_exception)} $message", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun initPlayer(index: Int) {
+        clear()
+        val resId = resources?.getIdentifier(
+            chapterList[index].strNameAudio, "raw", "jmapps.hadith40"
+        )
+        player = resId?.let { MediaPlayer.create(this, it) }
+        currentAudioProgress()
+        player?.setOnCompletionListener(this)
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        tbPlay.isChecked = false
+        handler.removeCallbacks(runnable)
+        sbAudioProgress?.progress = 0
+    }
+
+    private fun currentAudioProgress() {
+        sbAudioProgress?.max = player?.seconds!!
+        runnable = Runnable {
+            sbAudioProgress?.progress = player?.currentSeconds!!
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        if (fromUser) {
+            player?.seekTo(progress * 1000)
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+    private fun clear() {
+        player?.stop()
+        player?.release()
+        player = null
+    }
+
+    private val MediaPlayer.seconds: Int?
+        get() {
+            return this.duration / 1000
+        }
+
+    private val MediaPlayer.currentSeconds: Int?
+        get() {
+            return this.currentPosition / 1000
+        }
 }
